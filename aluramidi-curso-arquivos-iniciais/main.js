309e294d724171289ff91e3190993babd76de69c
docinho1 <113606429+docinho1@users.noreply.github.com>
2023-10-19 09:06:59 -0300

    Add files via upload

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -1,1 +1,8 @@
-alert('olá mundo');
+//Pom----------------------------------------------
+function tocaSomPom() {
+  document.querySelector('#som_tecla_pom').play();
+}
+
+const listaDeTeclas=document.querySelectorAll('.tecla');
+
+listaDeTeclas[0].onclick= tocaSomPom;
