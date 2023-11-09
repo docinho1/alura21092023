2f45e5b9af68037a0f4e6c650c3219ea9c2f6517
docinho1 <113606429+docinho1@users.noreply.github.com>
2023-11-09 08:52:54 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -6,16 +6,27 @@ function tocaSom(idElementoAudio) {
 const listaDeTeclas= document.querySelectorAll('.tecla');
 
   //para
- for ( let contador= 0; contador  < listaDeTeclas.length; contador ++){
-
-   const tecla = listaDeTeclas[contador];
-   const instrumento =Tecla.classList[1];
-   const idAudio =`#som_${instrumento}`; //template string
+ for ( let contador = 0; contador  < listaDeTeclas.length; contador++){
    
+const tecla = listaDeTeclas[contador];
+const instrumento =Tecla.classList[1];
+const idAudio =`#som_${instrumento}`; 
+
     Tecla.onclick= function(){ 
       tocaSom (idAudio);
     }
 
-   console.log(contador);
+    tecla.onkeydown = function (evento){
+
+      console.log(evento)
+
+      if ('espaço'){
+      tecla.classList.add('ativa');
+     }
+  }
+  
+    tecla.onkeyup = function (){
+      teclas.classList.remove('ativa');
+   }
 
 }
